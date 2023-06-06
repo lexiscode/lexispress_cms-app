ee89888b4cb20911c63cd4b771cb8361ae1b66f6
Nwokorie Alex V <nwokoriealex20@gmail.com>
2023-06-06 19:51:22 +0100

    feat: updated comments info

--- logout.php
+++ logout.php
@@ -1,14 +1,14 @@
 <?php
 
 // Initialize the session.
-// If you are using session_name("something"), don't forget it now!
 session_start();
 
 // Unset all of the session variables.
 $_SESSION = array();
 
 // If it's desired to kill the session, also delete the session cookie.
-// Note: This will destroy the session, and not just the session data!
+
+// This will delete the session cookie data!
 if (ini_get("session.use_cookies")) {
     $params = session_get_cookie_params();
     setcookie(session_name(), '', time() - 42000,
@@ -20,5 +20,6 @@ if (ini_get("session.use_cookies")) {
 // Finally, destroy the session.
 session_destroy();
 
+// Redirects the user to the homepage
 header('Location: index.php');
 exit;
