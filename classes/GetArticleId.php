2629c19067de32fb6a70e09ad724997241ee36a2
Nwokorie Alex V <nwokoriealex20@gmail.com>
2023-06-09 11:07:09 +0100

    feat: created GetArticleId.php

---
+++ classes/GetArticleId.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Article
+ * 
+ * A piece of writing for publication
+ */
+class get_article_id
+{
+    /**
+     * Get all the articles
+     * @param object $conn Connection to the database
+     * @return array An associative array of all the article records
+     */
+    public static function getAll($conn)
+    {
+        // READING FROM THE DATABASE AND CHECKING FOR ERRORS
+        $sql = "SELECT * 
+        FROM article
+        ORDER BY date_published DESC;";
+
+        // Execute the sql statement, returning a result set as a PDOStatement object
+        $results = $conn->query($sql); 
+
+        $articles = $results->fetchAll(PDO::FETCH_ASSOC);
+        //print_r($articles);  prints an associative array
+
+        return $articles;
+    }
+}
+
