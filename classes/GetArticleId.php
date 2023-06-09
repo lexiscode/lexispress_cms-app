b0e8d8a5cb64b91b8bddc0d29a119ad1368d134b
Nwokorie Alex V <nwokoriealex20@gmail.com>
2023-06-09 11:48:36 +0100

    feat: removed a method we no longer need

--- classes/GetArticleId.php
+++ classes/GetArticleId.php
@@ -11,29 +11,6 @@ class GetArticleId
     public $title;
     public $description;
     public $date_published;
-    
-    /**
-     * Get all the articles
-     * @param object $conn Connection to the database
-     * @return array An associative array of all the article records
-     */
-
-    public static function getAll($conn)
-    {
-        // READING FROM THE DATABASE AND CHECKING FOR ERRORS
-        $sql = "SELECT * 
-        FROM article
-        ORDER BY date_published DESC;";
-
-        // Execute the sql statement, returning a result set as a PDOStatement object
-        $results = $conn->query($sql); 
-
-        $articles = $results->fetchAll(PDO::FETCH_ASSOC);
-        //print_r($articles);  prints an associative array
-
-        return $articles;
-    }
-
 
     /**
      * Get the article record based on the ID
