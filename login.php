ed54aa141d39f8a1b6f47ac74a6549ccdce3067f
Nwokorie Alex V <nwokoriealex20@gmail.com>
2023-06-15 17:19:53 +0100

    feat: created a User class and reconfigured the authenticate method

--- login.php
+++ login.php
@@ -1,5 +1,7 @@
 <?php
 
+require "classes/User.php";
+
 // Initialize the session.
 session_start();
 
@@ -8,7 +10,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
     // Check if the submit button has been clicked, and check if the fields ain't empty also
     if (isset($_POST['sign-in'])){
         if (!empty($_POST['username']) && !empty($_POST['password'])){
-            if ($_POST['username'] == 'lexiscode' && $_POST['password'] == 'secret123'){
+            if (User::authenticate($_POST['username'], $_POST['password'])){
 
                 // this helps prevent session fixation attacks
                 session_regenerate_id(true);
