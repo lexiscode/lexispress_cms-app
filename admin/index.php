244922521bb689bdf470ce78d2a6a330a2c6c3fa
Nwokorie Alex V <nwokoriealex20@gmail.com>
2023-06-16 15:07:49 +0100

    feat: replaced the foreach loop to using tables

--- admin/index.php
+++ admin/index.php
@@ -9,6 +9,8 @@ require "classes/Auth.php";*/
 // Initialize the session.
 session_start();
 
+Auth::requireLogin();
+
 // Connect to the Database Server
 $conn = require "../includes/db.php";
 
@@ -31,7 +33,7 @@ $articles = GetAll::getAll($conn);
     <p>You are logged in. <a href="logout.php">Logout</a></p>
     <p>Welcome back!😊</p>
     <!-- only logged in user should access this link below-->
-    <!--<a href="new_article.php">New article</a>-->
+    <a href="new_article.php">New article</a>
 
 <?php else: ?>
     <p>You are logged out. <a href="login.php">Login</a></p>
@@ -39,22 +41,25 @@ $articles = GetAll::getAll($conn);
 
 <!--Below we want all users to see this New Article link, even though not everyone will be allowed
 to access it -->
-<a href="new_article.php">New article</a>
+<!--<a href="new_article.php">New article</a>-->
+
+<h2>Administration</h2>
 
 <?php if(!empty($articles)): ?>
-    <ul>
-        <?php foreach ($articles as $article): ?>
-            <li>
-                <article>
-                    <!-- htmlspecialchars() prevents XSS attack or code injections -->
-                    <h2><?= htmlspecialchars($article["title"]) ?></h2> 
-                    <p><?= htmlspecialchars($article["content"]) ?></p>
-                    <p><?= $article["date_published"]?></p>
-                    <i><a href="article.php?id=<?= $article['id'];?>">Read more</a></i>
-                </article>
-            </li>
-        <?php endforeach; ?>
-    </ul>
+    <table>
+        <thead>
+            <th>Title</th>
+        </thead>
+        <tbody>
+            <?php foreach ($articles as $article): ?>
+                <tr>
+                    <td>
+                        <a href="article.php?id=<?= $article['id'];?>"><?= htmlspecialchars($article['title']);?></a>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
 <?php else: ?>
     <p>No articles found.</p>
 <?php endif; ?>
